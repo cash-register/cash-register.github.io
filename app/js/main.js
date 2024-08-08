7b9a6d2516180bf33e5d6f8c5a7dcbcfea1c545f
lytsites <lyttern.lu@gmail.com>
2024-08-09 00:12:50 +0500

    com

--- app/js/main.js
+++ app/js/main.js
@@ -1,6 +1,30 @@
 $(document).ready(function () {
 	$('body').css('display', 'block');
 
+	// Переменные для вашего GitHub репозитория
+	const username = 'cash-register';	 // Замените на ваше имя пользователя
+	const repository = 'cash-register'; // Замените на ваш репозиторий
+	const branch = 'main';				// Замените на нужную ветку
+	const filename = 'jsonData.json'; // Путь к вашему JSON файлу
+
+	// Сформируйте URL сырого JSON файла из вашего GitHub репозитория
+	const jsonUrl = `https://raw.githubusercontent.com/${username}/${repository}/${branch}/${filename}`;
+
+	// Чтение данных из JSON файла и вывод в консоль
+	fetch(jsonUrl)
+		.then(response => {
+			if (!response.ok) {
+				throw new Error('Network response was not ok ' + response.statusText);
+			}
+			return response.json();
+		})
+		.then(data => {
+			console.log('Данные из JSON файла:', data);
+		})
+		.catch(error => {
+			console.error('Ошибка при получении JSON:', error);
+		});
+
 	$('.btn').on('click', function () {
 
 		const number = $('#number').val();
